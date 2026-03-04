d78372fcf77902e986b3525382487a1f0fff09e2
nikolaihoffmann-svg <nikolaihoffmann@hotmail.com>
2026-03-04 23:37:54 +0100

    Update Gjeld.tsx

--- src/pages/Gjeld.tsx
+++ src/pages/Gjeld.tsx
@@ -1,5 +1,11 @@
+// src/pages/Gjeld.tsx
 import React from "react";
 
 export function Gjeld() {
-  return <div className="card">Gjeld kommer…</div>;
+  return (
+    <div className="card">
+      <div className="cardTitle">Gjeld</div>
+      <div className="cardSub">Kommer i neste steg (forfalt/ikke forfalt + varsling).</div>
+    </div>
+  );
 }
