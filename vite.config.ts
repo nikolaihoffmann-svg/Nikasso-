767615e27c2ae1f79eaae88677c4ba22d5931e43
nikolaihoffmann-svg <nikolaihoffmann@hotmail.com>
2026-03-04 11:14:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages trenger base-path = /repo-navn/
-// Vi setter den via env i GitHub Actions (BASE_PATH)
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_PATH ?? "/",
+  base: "/Nikasso-/",
 });
