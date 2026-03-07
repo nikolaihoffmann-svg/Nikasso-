1eb363a1a0cce6f52a3cedea96385488ef7bd52d
nikolaihoffmann-svg <nikolaihoffmann@hotmail.com>
2026-03-07 01:08:57 +0100

    Update Backup.tsx

--- src/pages/Backup.tsx
+++ src/pages/Backup.tsx
@@ -1,26 +1,33 @@
 // src/pages/Backup.tsx
 import React from "react";
-import { clearAllData, downloadExportAll, pickImportAllFile } from "../app/storage";
+import { clearAllData, downloadExportAll, importAllFromFile, getStorageSummary, fmtKr } from "../app/storage";
 
 export function Backup() {
+  const s = getStorageSummary();
+
   return (
     <div className="card">
-      <div className="cardTitle">Backup</div>
-      <div className="cardSub">Eksporter/importer all data (varer, kunder, salg, gjeld, saldo).</div>
+      <div className="cardTitle">Data</div>
+      <div className="cardSub">Backup / import / nullstill (lokalt i nettleseren).</div>
+
+      <div className="list">
+        <div className="item">
+          <p className="itemTitle">Status</p>
+          <div className="itemMeta">
+            Varer: <b>{s.items}</b> • Kunder: <b>{s.customers}</b> • Salg: <b>{s.sales}</b> • Gjeld: <b>{s.receivables}</b>
+            <br />
+            Saldo: <b>{fmtKr(s.saldo)}</b>
+          </div>
+        </div>
+      </div>
 
       <div className="btnRow" style={{ marginTop: 12 }}>
-        <button className="btn btnPrimary" type="button" onClick={() => downloadExportAll()}>
+        <button className="btn btnPrimary" type="button" onClick={downloadExportAll}>
           Eksporter ALT
         </button>
-
-        <button className="btn" type="button" onClick={() => pickImportAllFile("replace")}>
-          Importer (erstatt)
+        <button className="btn" type="button" onClick={importAllFromFile}>
+          Importer ALT
         </button>
-
-        <button className="btn" type="button" onClick={() => pickImportAllFile("merge")}>
-          Importer (slå sammen)
-        </button>
-
         <button
           className="btn btnDanger"
           type="button"
@@ -30,6 +37,10 @@ export function Backup() {
         >
           Nullstill
         </button>
+      </div>
+
+      <div className="itemMeta" style={{ marginTop: 10 }}>
+        Tips: Ta alltid “Eksporter ALT” før du gjør store endringer i koden.
       </div>
     </div>
   );
