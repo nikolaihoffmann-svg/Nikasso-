4c44247b1293450f9a24c161fa141b6f96081964
nikolaihoffmann-svg <nikolaihoffmann@hotmail.com>
2026-03-25 04:51:26 +0100

    Update NewItemModal.tsx

--- src/components/NewItemModal.tsx
+++ src/components/NewItemModal.tsx
@@ -1,1 +1,21 @@
+import React from "react";
 
+type Props = {
+  open: boolean;
+  initialName?: string;
+  onClose: () => void;
+  onCreated: (item: unknown) => void;
+};
+
+export default function NewItemModal({ open, onClose }: Props) {
+  if (!open) return null;
+
+  return (
+    <div>
+      <div>Ny vare</div>
+      <button type="button" onClick={onClose}>
+        Lukk
+      </button>
+    </div>
+  );
+}
