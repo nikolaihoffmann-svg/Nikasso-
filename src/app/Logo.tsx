c2b192216ef29815f9e6bd93d9af3a66622ae1f9
nikolaihoffmann-svg <nikolaihoffmann@hotmail.com>
2026-03-29 05:35:27 +0200

    Create Logo.tsx

---
+++ src/app/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="brand">
+      <span className="brandText">Nikasso</span>
+      <span className="brandPlus">+</span>
+    </div>
+  );
+}
