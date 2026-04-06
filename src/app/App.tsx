df6df3f2d5215621c6f4c3de6b0e43f22fecc026
nikolaihoffmann-svg <nikolaihoffmann@hotmail.com>
2026-04-06 21:07:39 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{ minHeight: "100vh", background: "#08111f", color: "#fff", padding: 24 }}>
-      TEST APP OK
+      REACT TEST OK
     </div>
   );
-} 
+}
